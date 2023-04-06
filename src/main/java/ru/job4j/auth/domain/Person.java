097e3c7f640108474ec5b94fee4a670e5486ee60
svoh86 <sms-86@mail.ru>
2023-04-06 15:13:07 +0300

    change pom

--- src/main/java/ru/job4j/auth/domain/Person.java
+++ src/main/java/ru/job4j/auth/domain/Person.java
@@ -20,6 +20,7 @@ import javax.persistence.*;
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private int id;
     private String login;
     private String password;
